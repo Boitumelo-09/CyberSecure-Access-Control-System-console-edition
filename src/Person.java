aff9c842947e04734261da5b20648fd000d81bd2
boitumelo-09 <belmiredub4@gmail.com>
2026-02-14 05:35:02 +0200

    feat : "Add getters & setters for mandatory class fields"

---
+++ src/Person.java
@@ -0,0 +1,32 @@
+public class Person {
+
+   private String name;
+   private String Phone;
+   private String email;
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return Phone;
+    }
+
+    public void setPhone(String phone) {
+        Phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
