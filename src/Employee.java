1bd3665630ddd2db5fb862f567c326bc2442b8e2
boitumelo-09 <belmiredub4@gmail.com>
2026-02-14 05:52:10 +0200

    feat : "Add getters & setters for mandatory Employee class fields"

---
+++ src/Employee.java
@@ -0,0 +1,33 @@
+
+
+public abstract class Employee extends Person{
+       private String employeeID;
+       private int clearanceLevel;
+       private String jobTitle;
+
+    public Employee(String name,String employeeID) {
+        super(name);
+        this.employeeID = employeeID;
+    }
+
+    public String getEmployeeID() {
+        return employeeID;
+    }
+    public void setEmployeeID(String employeeID) {
+        this.employeeID = employeeID;
+    }
+    public int getClearanceLevel() {
+        return clearanceLevel;
+    }
+    public void setClearanceLevel(int clearanceLevel) {
+        this.clearanceLevel = clearanceLevel;
+    }
+    public String getJobTitle() {
+        return jobTitle;
+    }
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+
+}
