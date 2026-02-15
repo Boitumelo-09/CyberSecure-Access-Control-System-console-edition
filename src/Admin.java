11db185798e6a9b1a631fd50db3dac00974204c5
boitumelo-09 <belmiredub4@gmail.com>
2026-02-15 21:45:35 +0200

    fix : "Implemented The Admin Class Method For Accessing The System As An Administrator..."

---
+++ src/Admin.java
@@ -0,0 +1,19 @@
+
+
+public class Admin extends Employee implements AuthenticationInterface {
+
+
+    public Admin(String name, String employeeID) {
+        super(name, employeeID);
+    }
+
+    @Override
+    public boolean userAuthentication(String userName, String userID) {
+        return false;
+    }
+
+    @Override
+    public void accessSystem() {
+             IO.println("Admin Entering System...")
+    }
+}
