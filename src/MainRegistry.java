a9822be2901f5ec2f27a0d9821dadf14ad9c70be
boitumelo-09 <belmiredub4@gmail.com>
2026-02-18 06:16:44 +0200

    Feat: "Implemented A LIST to store registered employees"

--- src/MainRegistry.java
+++ src/MainRegistry.java
@@ -1,3 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainRegistry {
 
+    private List<Employee> registeredEmployees = new ArrayList<>();
+
+        public MainRegistry(){
+            registeredEmployees.add(new Admin("Rosabel","3652"));
+            registeredEmployees.add(new Intern("Kabob Teka","INT345"));
+            registeredEmployees.add(new SecurityAnalyst("Boitumelo","SOC5643"));
+        }
+
+
+
+    public List<Employee> getRegisteredEmployees() {
+        return registeredEmployees;
+    }
+
+    public void setRegisteredEmployees(List<Employee> registeredEmployees) {
+        this.registeredEmployees = registeredEmployees;
+    }
 }
