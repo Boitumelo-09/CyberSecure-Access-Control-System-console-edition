e95db2ec7ef014b55fef9290f9901e338515bcc5
boitumelo-09 <belmiredub4@gmail.com>
2026-02-25 05:51:10 +0200

    Feat: "Introducing a facade design pattern by adding a "System" interface implemented by the Main registry class"

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,8 @@
 
 void main() throws Exception {
 
-
-
-
  System system = new System();
  system.run();
-
 
     IO.println("Closing Environment...");
 }
--- src/System.java
+++ src/System.java
@@ -1,4 +1,6 @@
+import java.util.*;
 public class System {
+
 
 
 
@@ -11,6 +13,8 @@ public class System {
           IO.print("Name : "); userName = IO.readln();
           IO.print("Identification : "); userID = IO.readln();
          portal.login(userName,userID);
+        IO.println("CLosing SySTEM");
+
     }
 
 
